5e4c4cacc5a1c875b20bae05da57c75556117537
sammyteahan <sammyteahan@gmail.com>
2017-11-08 07:37:41 -0700

    Formatting with elm-format

--- Main.elm
+++ Main.elm
@@ -2,15 +2,11 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
 import Json.Decode as Json exposing (..)
 import String
 import Http
-import Task
-
 import Material
 import Material.List as Lists
-import Material.Grid as Grid
 import Material.Color as Color
 import Material.Layout as Layout
 import Material.Scheme as Scheme
@@ -19,103 +15,116 @@ import Material.Spinner as Loading
 import Material.Textfield as Textfield
 import Material.Options as Options exposing (css)
 import Json.Decode.Pipeline as JsonPipeline exposing (decode, required, requiredAt)
-import Json.Decode.Extra exposing ((|:))
 
 
 -- Model(s)
+
+
 type alias Post =
-  { title : String
-  , url : String
-  , domain : String
-  }
+    { title : String
+    , url : String
+    , domain : String
+    }
+
 
 type alias Model =
-  { searchString : String
-  , selectedReddit : String
-  , fetching : Bool
-  , fetchError : String
-  , imgUrl : String
-  , posts : List Post
-  , mdl : Material.Model
-  }
+    { searchString : String
+    , selectedReddit : String
+    , fetching : Bool
+    , fetchError : String
+    , imgUrl : String
+    , posts : List Post
+    , mdl : Material.Model
+    }
+
 
 model : Model
 model =
-  { searchString = ""
-  , selectedReddit = "elm"
-  , fetching = True
-  , fetchError = ""
-  , imgUrl = "loading.gif"
-  , posts = []
-  , mdl = Material.model
-  }
+    { searchString = ""
+    , selectedReddit = "elm"
+    , fetching = True
+    , fetchError = ""
+    , imgUrl = "loading.gif"
+    , posts = []
+    , mdl = Material.model
+    }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-    ( model, getSubReddit "elm")
+    ( model, getSubReddit "elm" )
+
 
 
 -- Update
+
+
 type Msg
-  = UpdateSearchString String
-  | GetSubreddit
-  | NewSubreddit (Result Http.Error (List Post))
-  | Mdl (Material.Msg Msg)
+    = UpdateSearchString String
+    | GetSubreddit
+    | NewSubreddit (Result Http.Error (List Post))
+    | Mdl (Material.Msg Msg)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    UpdateSearchString newString ->
-      { model | searchString = newString } ! []
+    case msg of
+        UpdateSearchString newString ->
+            { model | searchString = newString } ! []
 
-    -- todo: there's a better way to apply these changes
-    GetSubreddit ->
-      if String.isEmpty model.searchString then
-        model ! []
-      else
-        if model.selectedReddit == model.searchString then
-          model ! []
-        else
-          { model | fetching = True, selectedReddit = model.searchString }
-          ! [ getSubReddit model.searchString ]
+        GetSubreddit ->
+            if String.isEmpty model.searchString then
+                model ! []
+            else if model.selectedReddit == model.searchString then
+                model ! []
+            else
+                { model | fetching = True, selectedReddit = model.searchString }
+                    ! [ getSubReddit model.searchString ]
 
-    NewSubreddit (Ok posts) ->
-      { model | posts = posts, fetching = False } ! []
+        NewSubreddit (Ok posts) ->
+            { model | posts = posts, fetching = False } ! []
 
-    NewSubreddit (Err _) ->
-      let
-        errorMessage =
-          "Some went wrong 😳"
-      in
-        { model | searchString = errorMessage, fetching = False } ! []
+        NewSubreddit (Err _) ->
+            let
+                errorMessage =
+                    "Some went wrong 😳"
+            in
+                { model | searchString = errorMessage, fetching = False } ! []
 
-    Mdl action_ ->
-      Material.update Mdl action_ model
+        Mdl action_ ->
+            Material.update Mdl action_ model
+
 
 
 -- View
 -- Don't use 'Schem.top..' in prod. It does handle some style/Scheme
 -- related stuff, but all it really does is inject google stylesheets
 -- into a header script, which should be done in index.html
+
+
 view : Model -> Html Msg
 view model =
-  Scheme.topWithScheme Color.Teal Color.LightGreen
-    <| Layout.render Mdl
-      model.mdl
-      [ Layout.fixedHeader
-      ]
-      { header = [ header ]
-      , drawer = []
-      , tabs = ( [], [] )
-      , main = [ viewContent model ]
-      }
+    Scheme.topWithScheme Color.Teal Color.LightGreen <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            ]
+            { header = [ header ]
+            , drawer = []
+            , tabs = ( [], [] )
+            , main = [ viewContent model ]
+            }
+
+
 
 -- TODO render menu and search bar
+
+
 header : Html Msg
 header =
-  div []
-    [ h4 [ style [("float", "left"), ("padding-left", "20px")]] [ text "Reddit" ]
-    ]
+    div []
+        [ h4 [ style [ ( "float", "left" ), ( "padding-left", "20px" ) ] ] [ text "Reddit" ]
+        ]
 
 
 containerStyle : List (Options.Property a b)
@@ -126,101 +135,128 @@ containerStyle =
     , css "padding-top" "25px"
     ]
 
+
+
 -- TODO disable GetSubreddit action if textfield is empty
+
+
 viewContent : Model -> Html Msg
 viewContent model =
-  Options.div containerStyle
-    [ Textfield.render Mdl [1] model.mdl
-      [ Options.onInput UpdateSearchString
-      ]
-      []
-    , Button.render Mdl [2] model.mdl
-      [ Button.raised
-      , Button.ripple
-      , Button.colored
-      , Options.onClick GetSubreddit
-      , css "margin-left" "10px"
-      ]
-      [ text "Fetch Subreddit" ]
-    , br [] []
-    , h2 [] [ text model.selectedReddit ]
-    , div [ class "wrap-posts" ]
-      [ section []
-        [ listContent model ]
-      ]
-    ]
+    Options.div containerStyle
+        [ Textfield.render Mdl
+            [ 1 ]
+            model.mdl
+            [ Options.onInput UpdateSearchString
+            ]
+            []
+        , Button.render Mdl
+            [ 2 ]
+            model.mdl
+            [ Button.raised
+            , Button.ripple
+            , Button.colored
+            , Options.onClick GetSubreddit
+            , css "margin-left" "10px"
+            ]
+            [ text "Fetch Subreddit" ]
+        , br [] []
+        , h2 [] [ text model.selectedReddit ]
+        , div [ class "wrap-posts" ]
+            [ section []
+                [ listContent model ]
+            ]
+        ]
+
 
 renderSpinner : Html Msg
 renderSpinner =
-  Loading.spinner [ Loading.active True ]
+    Loading.spinner [ Loading.active True ]
+
 
 listContent : Model -> Html Msg
 listContent model =
-  let
-    renderContent =
-      if model.fetching then
-        renderSpinner
-      else
-        div []
-          [ Lists.ul []
-            (List.map listView model.posts)
-          ]
-  in
-    renderContent
+    let
+        renderContent =
+            if model.fetching then
+                renderSpinner
+            else
+                div []
+                    [ Lists.ul []
+                        (List.map listView model.posts)
+                    ]
+    in
+        renderContent
+
 
 listView : Post -> Html Msg
 listView post =
-  Lists.li []
-    [ Lists.content []
-      [ a [ style [ ("color", "rgba(0, 0, 0, 0.87)")
-                  , ("text-decoration", "none")
-                  , ("font-weight", "400")
-                  ]
-          , href post.url
-          , target "_blank"
-          ]
-          [ text post.title ]
+    Lists.li []
+        [ Lists.content []
+            [ a
+                [ style
+                    [ ( "color", "rgba(0, 0, 0, 0.87)" )
+                    , ( "text-decoration", "none" )
+                    , ( "font-weight", "400" )
+                    ]
+                , href post.url
+                , target "_blank"
+                ]
+                [ text post.title ]
+            ]
         ]
-    ]
+
 
 loadingView : Model -> Html Msg
 loadingView model =
-  div []
-    [ text "loading" ]
+    div []
+        [ text "loading" ]
+
 
 
 -- Subscriptions
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- Http
+
+
 getSubReddit : String -> Cmd Msg
 getSubReddit searchString =
-  let
-    url = "https://www.reddit.com/r/" ++ searchString ++ ".json"
-  in
-    Http.send NewSubreddit (Http.get url decodePosts)
+    let
+        url =
+            "https://www.reddit.com/r/" ++ searchString ++ ".json"
+    in
+        Http.send NewSubreddit (Http.get url decodePosts)
+
 
 decodePost : Json.Decoder Post
 decodePost =
-  decode Post
-    |> requiredAt ["data", "title"] Json.string
-    |> requiredAt ["data", "url"] Json.string
-    |> requiredAt ["data", "domain"] Json.string
+    decode Post
+        |> requiredAt [ "data", "title" ] Json.string
+        |> requiredAt [ "data", "url" ] Json.string
+        |> requiredAt [ "data", "domain" ] Json.string
+
 
 decodePosts : Json.Decoder (List Post)
 decodePosts =
-  Json.list decodePost
-    |> Json.at ["data", "children"]
+    Json.list decodePost
+        |> Json.at [ "data", "children" ]
+
 
 
 -- Main
+
+
+main : Program Never Model Msg
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
